4cd7ab87a4867e9df28928ace228fea6241d1a64
victor.maia <victor.maia@c040.corp.cerc.inf.br>
2020-05-19 00:19:06 -0300

    Primeiro commit

---
+++ adm/src/main/java/com/eusugiro/adm/AdmApplication.java
@@ -0,0 +1,13 @@
+package com.eusugiro.adm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmApplication.class, args);
+	}
+
+}
---
+++ adm/src/test/java/com/eusugiro/adm/AdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eusugiro.adm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
